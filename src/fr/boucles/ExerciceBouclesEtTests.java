fdd5172be97bf2660625e66fd138cae2d3e79915
Fromont-L <lucasfromont97@gmail.com>
2025-03-21 17:36:20 +0100

    Update ExerciceBouclesEtTest.java

--- src/fr/boucles/ExerciceBouclesEtTests.java
+++ src/fr/boucles/ExerciceBouclesEtTests.java
@@ -5,8 +5,8 @@ public class ExerciceBouclesEtTests {
         int[] array = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4};
 
         System.out.println("Afficher l'ensemble des éléments du tableau");
-        for (int i = 0; i < array.length; i++) {
-            System.out.println(array[i]);
+        for (int j : array) {
+            System.out.println(j);
         }
 
         System.out.println("Afficher l'ensemble des éléments du tableau à l'envers");
